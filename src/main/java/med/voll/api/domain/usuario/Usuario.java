3832ad6d418a7c098c2f29e586ea660442b7f16f
gabrielevarao <gvarao93@gmail.com>
2025-07-31 17:11:23 -0300

    Feat: creating Usuario entity class in order to implement authentication

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+
+}
