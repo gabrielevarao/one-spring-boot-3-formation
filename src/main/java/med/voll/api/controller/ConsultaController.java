c957acf787c84cbc536172779a800eeac28f4036
gabrielevarao <gvarao93@gmail.com>
2025-08-01 17:43:07 -0300

    Feat: creating an entity class for appointments called Consulta

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,17 @@
+package med.voll.api.controller;
+
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+
+}
