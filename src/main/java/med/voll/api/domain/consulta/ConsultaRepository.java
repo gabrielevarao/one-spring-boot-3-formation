fb1e658bf98a13da4f9056c3bd942107f5b7c42c
gabrielevarao <gvarao93@gmail.com>
2025-08-01 20:37:12 -0300

    Feat: implementing a method to select a random doctor by specialty and availability
    At MedicoRepository: new JPQL query to select the random doctor using named parameters. At ConsultaService: new method calling the query

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,8 +1,14 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.medico.Especialidade;
+import med.voll.api.domain.medico.Medico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
 }
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -3,11 +3,26 @@ package med.voll.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+            SELECT m FROM Medico m
+            WHERE m.ativo = true
+            AND m.especialidade = :especialidade
+            AND m.id NOT IN(
+                SELECT c.medico.id FROM Consulta c
+                WHERE c.data = :data)
+            ORDER BY RAND()
+            LIMIT 1
+            """)
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 }
 
