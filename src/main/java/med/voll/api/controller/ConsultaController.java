5eec2753ae4a4b3f379a8f6efa1d5fed4439f3c2
gabrielevarao <gvarao93@gmail.com>
2025-08-01 17:56:57 -0300

    Feat: creating controller class for Consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -4,6 +4,7 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,8 +17,8 @@ public class ConsultaController {
 
     @PostMapping
     @Transactional
-    public void agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
-
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
 }
